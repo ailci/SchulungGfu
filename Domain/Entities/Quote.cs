ed2f267f65a8336b711e75f87eaa76bbc3285b0b
Ali Ilci <Ali.Ilci@gmx.de>
2025-06-30 15:15:46 +0200

    ViewModel

---
+++ Domain/Entities/Quote.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Quote
+{
+    public Guid Id { get; set; }  // ID,Id, QuoteId
+    public required string QuoteText { get; set; }
+    public Author? Author { get; set; }
+    public Guid AuthorId { get; set; } //FK => Author + Id
+}
