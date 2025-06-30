2b3a2f61d02f360e8a0de2db40a1b3d7718cfb0e
Ali Ilci <Ali.Ilci@gmx.de>
2025-06-30 13:45:17 +0200

    Config

---
+++ UI.Blazor/UI.Blazor/Configuration/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UI.Blazor.Components.Account;
+using UI.Blazor.Data;
+
+namespace UI.Blazor.Configuration;
+
+public  static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder AddBlazorConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents(options =>
+            {
+                //Detaillierte Fehlermeldung bei Circuit
+                options.DetailedErrors = builder.Environment.IsDevelopment();
+
+            }) // Blazor Server
+            .AddInteractiveWebAssemblyComponents() // WebAssembly
+            .AddAuthenticationStateSerialization();
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddAuthentificationConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCascadingAuthenticationState();
+        builder.Services.AddScoped<IdentityUserAccessor>();
+        builder.Services.AddScoped<IdentityRedirectManager>();
+        builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddIdentityCookies();
+
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+        return builder;
+    }
+}
--- UI.Blazor/UI.Blazor/Program.cs
+++ UI.Blazor/UI.Blazor/Program.cs
@@ -4,40 +4,15 @@ using Microsoft.EntityFrameworkCore;
 using UI.Blazor.Client.Pages;
 using UI.Blazor.Components;
 using UI.Blazor.Components.Account;
+using UI.Blazor.Configuration;
 using UI.Blazor.Data;
 using UI.Blazor.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents() // Blazor Server
-    .AddInteractiveWebAssemblyComponents() // WebAssembly
-    .AddAuthenticationStateSerialization();
-
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityUserAccessor>();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+builder.AddBlazorConfig()
+    .AddAuthentificationConfig();
 
 var app = builder.Build();
 
