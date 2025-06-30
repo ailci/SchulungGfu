d698d549d5871093a4c614f717affdf61ca150c2
Ali Ilci <Ali.Ilci@gmx.de>
2025-06-30 16:01:29 +0200

    Quote

--- UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
@@ -1,5 +1,18 @@
+using Application.ViewModels.Qotd;
+
 namespace UI.Blazor.Components.Pages;
 public partial class Home
 {
+    public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
+    protected override void OnInitialized()
+    {
+        QotdViewModel = new QuoteOfTheDayViewModel
+        {
+            AuthorName = "Ich",
+            AuthorDescription = "Dozent",
+            QuoteText = "Larum lierum Löffelstiel",
+            AuthorBirthDate = new DateOnly(1978, 07, 13)
+        };
+    }
 }
