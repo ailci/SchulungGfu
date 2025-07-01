b14834c0497c609e9e4b7249cd6974a568d128cf
Ali Ilci <Ali.Ilci@gmx.de>
2025-07-01 10:36:53 +0200

    EF

--- UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
@@ -1,12 +1,18 @@
 using Application.ViewModels.Qotd;
+using Microsoft.AspNetCore.Components;
+using Persistence;
 
 namespace UI.Blazor.Components.Pages;
 public partial class Home
 {
+    [Inject] public QotdContext QotdContext { get; set; } = null!;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
     protected override void OnInitialized()
     {
+        var quotes = QotdContext.Quotes.ToList();
+
+
         QotdViewModel = new QuoteOfTheDayViewModel
         {
             AuthorName = "Ich",
