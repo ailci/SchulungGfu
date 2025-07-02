8ff640f6e1ea68c1b5cd58d3427cd83527339f9d
Ali Ilci <Ali.Ilci@gmx.de>
2025-07-02 15:53:35 +0200

    Confirm Dialog to be continued...

--- UI.Blazor.ComponentsLibrary/Components/ConfirmDialog.razor.cs
+++ UI.Blazor.ComponentsLibrary/Components/ConfirmDialog.razor.cs
@@ -1,6 +1,8 @@
 namespace UI.Blazor.ComponentsLibrary.Components;
 public partial class ConfirmDialog
 {
+    private bool _showConfirm;
+
     private Task OnConfirmChange(bool deleteConfirmed)
     {
         throw new NotImplementedException();
