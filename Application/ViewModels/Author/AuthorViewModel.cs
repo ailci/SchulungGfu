659713f7e92a63cde3990d99a8a2f6692527f267
Ali Ilci <Ali.Ilci@gmx.de>
2025-07-03 10:22:26 +0200

    AuthorDetails

--- Application/ViewModels/Author/AuthorViewModel.cs
+++ Application/ViewModels/Author/AuthorViewModel.cs
@@ -14,7 +14,7 @@ public class AuthorViewModel
     public required string Name { get; set; }
     public required string Description { get; set; }
     public DateOnly? BirthDate { get; set; }
-    //public IList<QuoteViewModel> Quotes { get; set; } = [];
+    public IList<QuoteViewModel> Quotes { get; set; } = [];
     public byte[]? Photo { get; set; }
     public string? PhotoMimeType { get; set; }
 }
