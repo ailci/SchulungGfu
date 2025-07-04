249375b81f310475751363635b034a5346163f1e
Ali Ilci <Ali.Ilci@gmx.de>
2025-07-04 13:26:54 +0200

    Serilog

---
+++ Logging/LoggerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Logging;
+
+public class LoggerManager(ILogger logger) : ILoggerManager
+{
+    public void LogDebug(string message) => logger.Debug(message);
+
+    public void LogError(string message) => logger.Error(message);
+
+    public void LogInformation(string message) => logger.Information(message);
+
+    public void LogWarning(string message) => logger.Warning(message);
+}
---
+++ Logging/LoggingServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Logging;
+
+public static class LoggingServiceRegistration
+{
+    public static WebApplicationBuilder AddLoggingConfig(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
+
+        builder.Host.UseSerilog((hostContext, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(hostContext.Configuration);
+        });
+
+        return builder;
+    }
+}
